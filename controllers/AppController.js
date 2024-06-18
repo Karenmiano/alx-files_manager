f69cadf8d0650d02c0636fed9689d3a21f144b7b
Karenmiano <mianokaren1798@gmail.com>
2024-06-18 16:47:48 +0300

    First api

---
+++ controllers/AppController.js
@@ -0,0 +1,25 @@
+import redisClient from "../utils/redis";
+import dbClient from "../utils/db";
+
+export default class AppController {
+  static getStatus(request, response) {
+    response.send({
+      "redis": redisClient.isAlive(),
+      "db": dbClient.isAlive(),
+    });
+  }
+
+  static getStats(request, response) {
+    async function stats() {
+      const userCount = await dbClient.nbUsers();
+      const fileCount = await dbClient.nbFiles();
+
+      response.send({
+        "users": userCount,
+        "files": fileCount,
+      });
+    }
+
+    stats();
+  }
+}
